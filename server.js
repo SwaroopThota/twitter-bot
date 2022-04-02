344bbacbd7968dc55f8bd61659c08bc8a65675e1
SwaroopThota <swaroopthota2001@gmail.com>
2022-04-02 18:32:35 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,19 @@
+const { default: axios } = require("axios");
+const client = require("./twitterClient");
+const schedule = require("node-schedule");
+const url = "https://programming-quotes-api.herokuapp.com/quotes/random";
+
+const tweet = async () => {
+  try {
+    const res = await axios.get(url);
+    const quote = `${res.data.en}\n    -${res.data.author}`;
+    await client.v2.tweet(quote);
+    console.log("You just tweeted '" + quote + "'");
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+schedule.scheduleJob("0 0 * * *", () => {
+  tweet();
+});
